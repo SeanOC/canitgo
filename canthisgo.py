7764774a8856bb8cda4243d0da0a480673c24dd0
Sean O'Connor <sean@bit.ly>
2017-02-05 16:12:54 -0500

    First stab at handling back and forth conversation.

--- canthisgo.py
+++ canthisgo.py
@@ -18,20 +18,85 @@ def new_query():
 
 @ask.intent('OvenIntent')
 def oven_start():
-	oven_start_msg = render_template('oven_start')
-	return statement(oven_start_msg)
+    oven_start_msg = render_template('oven_start')
+    session.attributes['DEVICE'] = 'oven'
+    session.attributes['YES_NEXT'] = 'done_positive'
+    session.attributes['YES_IS_DONE'] = True
+    session.attributes['NO_NEXT'] = 'composition_question'
+    session.attributes['NO_IS_DONE'] = False
+    session.attributes['YES_NO_STEP'] = 0
+    return question(oven_start_msg)
 
 
 @ask.intent('MicrowaveIntent')
-def oven_start():
-	oven_start_msg = render_template('microwave_start')
-	return statement(microwave_start_msg)
+def microwave_start():
+    microwave_start_msg = render_template('microwave_start')
+    session.attributes['DEVICE'] = 'microwave'
+    session.attributes['YES_NEXT'] = 'done_positive'
+    session.attributes['YES_IS_DONE'] = True
+    session.attributes['NO_NEXT'] = 'metal_question'
+    session.attributes['NO_IS_DONE'] = False
+    session.attributes['YES_NO_STEP'] = 0
+    return question(microwave_start_msg)
 
 
 @ask.intent('DishwasherIntent')
-def oven_start():
-	diswasher_start_msg = render_template('dishwasher_start')
-	return statement(diswasher_start_msg)
+def dishwasher_start():
+    diswasher_start_msg = render_template('dishwasher_start')
+    session.attributes['DEVICE'] = 'dishwasher'
+    session.attributes['YES_NEXT'] = 'done_positive'
+    session.attributes['YES_IS_DONE'] = True
+    session.attributes['NO_NEXT'] = 'composition_question'
+    session.attributes['NO_IS_DONE'] = False
+    session.attributes['YES_NO_STEP'] = 0
+    return question(diswasher_start_msg)
+
+
+@ask.intent('AffirmativeIntent')
+def affirmative():
+    device = session.attributes['DEVICE']
+    yes_next = session.attributes['YES_NEXT']
+    yes_is_done = session.attributes['YES_IS_DONE']
+    yes_no_step = session.attributes['YES_NO_STEP']
+
+    if yes_no_step == 1:
+        if device == 'microwave':
+            yes_next = 'metal_done'
+            yes_is_done = True
+
+    template = '_'.join((device, yes_next))
+    response_msg = render_template(template)
+
+
+    session.attributes['YES_NO_STEP'] = yes_no_step + 1
+    if yes_is_done:
+        return statement(response_msg)
+    else:
+        return question(response_msg)
+
+
+@ask.intent('NegativeIntent')
+def negative():
+    device = session.attributes['DEVICE']
+    no_next = session.attributes['NO_NEXT']
+    no_is_done = session.attributes['NO_IS_DONE']
+    yes_no_step = session.attributes['YES_NO_STEP']
+
+
+    if yes_no_step == 1:
+        if device == 'microwave':
+            no_next = 'plastic_start'
+            no_is_done = False
+
+    logging.info('device:  {}     no_next:  {}      yes_no_step:  {}'.format(device, no_next, yes_no_step))
+    template = '_'.join((device, no_next))
+    response_msg = render_template(template)
+    
+    session.attributes['YES_NO_STEP'] = yes_no_step + 1
+    if no_is_done:
+        return statement(response_msg)
+    else:
+        return question(response_msg)
 
 # @ask.intent('OvenIntent')
 # def oven():
